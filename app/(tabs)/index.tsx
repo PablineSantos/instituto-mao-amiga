54bbb4aa78b3f1a8b0ed559ee84d5c556c2f694a
PablineSantos <pablines2006@gmail.com>
2026-08-10 21:16:01 -0300

    refs #03

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,22 +1,41 @@
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
+import { View, Text } from "react-native";
+type Ponto = {
+  id: number;
+  nome: string;
+  quantidadeAlimentos: number;
+};
+const pontosMock: Ponto[] = [
+  {
+    id: 1,
+    nome: "Alfa",
+    quantidadeAlimentos: 1000,
+  },
+  {
+    id: 2,
+    nome: "Beta",
+    quantidadeAlimentos: 980,
+  },
+  {
+    id: 3,
+    nome: "sigma",
+    quantidadeAlimentos: 190,
+  },
+];
+function PontoItem({ ponto }: { ponto: Ponto }) {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello World!</Text>
+    <View>
+      <Text>{ponto.id}</Text>
+      <Text>{ponto.nome}</Text>
+      <Text>{ponto.quantidadeAlimentos}</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    fontSize: 24,
-    fontWeight: 'bold',
-  },
-});
+export default function TelaListaPontos() {
+  return (
+    <View>
+      {pontosMock.map((ponto) => (
+        <PontoItem key={ponto.id} ponto={ponto} />
+      ))}
+    </View>
+  );
+}
